e718afe2ce188832422d10a714d5559d37ad6e8f
krishnakhandelwalbtech2025-spec <krishna.khandelwal.btech2025@sitpune.edu.in>
2026-08-10 15:46:43 +0530

    Update StudentConstructorDemo.java

--- Assignment02/StudentConstructorDemo.java
+++ Assignment02/StudentConstructorDemo.java
@@ -1,34 +1,26 @@
-public class StudentConstructorDemo {
-    public static void main(String[] args) {
-        Student s1 = new Student();
-        Student s2 = new Student("Krishna Sharma", 21);
-
-        System.out.println("Student 1 (Default Constructor):");
-        s1.display();
-
-        System.out.println("\nStudent 2 (Parameterized Constructor):");
-        s2.display();
-    }
-}
-
-class Student {
-    String name;
-    int rollNumber;
-
-    // default constructor
-    Student() {
-        name = "Not Assigned";
-        rollNumber = 0;
-    }
-
-    // parameterized constructor
-    Student(String name, int rollNumber) {
-        this.name = name;
-        this.rollNumber = rollNumber;
-    }
-
-    void display() {
-        System.out.println("Name        : " + name);
-        System.out.println("Roll Number : " + rollNumber);
-    }
-}
+class Student { 
+private String name; 
+private int rollNumber; 
+public Student() { 
+this.name = "Unknown"; 
+this.rollNumber = 0; 
+}  
+public Student(String name, int rollNumber) { 
+this.name = name; 
+this.rollNumber = rollNumber; 
+}  
+public void display() { 
+System.out.println("Student Name : " + name); 
+System.out.println("Roll Number  : " + rollNumber); 
+System.out.println("---------------------------"); 
+} 
+} 
+public class StudentDemo { 
+public static void main(String[] args) { 
+Student student1 = new Student(); 
+Student student2 = new Student("Krishna Khandelwal", 101); 
+System.out.println("--- Student Details ---"); 
+student1.display(); 
+student2.display(); 
+} 
+} 
