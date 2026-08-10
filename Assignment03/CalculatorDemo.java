3322c1c8541dc73fafe52bb28dd683484286686b
krishnakhandelwalbtech2025-spec <krishna.khandelwal.btech2025@sitpune.edu.in>
2026-08-10 15:48:33 +0530

    Update CalculatorDemo.java

--- Assignment03/CalculatorDemo.java
+++ Assignment03/CalculatorDemo.java
@@ -1,26 +1,31 @@
-public class CalculatorDemo {
-    public static void main(String[] args) {
-        Calculator calc = new Calculator();
-
-        int intResult = calc.add(10, 20);
-        double doubleResult = calc.add(10.5, 20.3);
-
-        System.out.println("Sum of integers : " + intResult);
-        System.out.println("Sum of decimals : " + doubleResult);
-        System.out.println("Total Calculations Performed : " + Calculator.calculationCount);
-    }
-}
-
-class Calculator {
-    static int calculationCount = 0;   // static variable shared by all objects
-
-    int add(int a, int b) {
-        calculationCount++;
-        return a + b;
-    }
-
-    double add(double a, double b) {
-        calculationCount++;
-        return a + b;
-    }
+public class Calculator { 
+private static int calculationCount = 0; 
+public int add(int a, int b) { 
+calculationCount++; 
+return a + b; 
+} 
+public int add(int a, int b, int c) { 
+calculationCount++; 
+return a + b + c; 
+} 
+public double add(double a, double b) { 
+calculationCount++; 
+return a + b; 
+} 
+public double add(double a, double b, double c) { 
+calculationCount++; 
+return a + b + c; 
+} 
+public static int getCalculationCount() { 
+return calculationCount; 
+} 
+public static void main(String[] args) { 
+Calculator calc = new Calculator(); 
+System.out.println("Sum of 2 integers (10, 20): " + calc.add(10, 20)); 
+System.out.println("Sum of 3 integers (5, 15, 25): " + calc.add(5, 15, 25)); 
+System.out.println("Sum of 2 decimals (10.5, 20.25): " + calc.add(10.5, 20.25)); 
+System.out.println("Sum of 3 decimals (1.1, 2.2, 3.3): " + calc.add(1.1, 2.2, 3.3)); 
+System.out.println("\nTotal 
+Calculator.getCalculationCount()); 
+} 
 }
